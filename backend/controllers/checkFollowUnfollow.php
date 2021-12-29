562bee77f977a3e3f75a8dd9ed850beae9e4e416
Pedro <pedromarques119@gmail.com>
2021-12-29 18:19:43 +0000

    Acertos no front end

--- backend/controllers/checkFollowUnfollow.php
+++ backend/controllers/checkFollowUnfollow.php
@@ -47,41 +47,6 @@
         }
     } 
 
-    
-    // if($_SERVER["REQUEST_METHOD"] === "POST"){
-
-    //     $connectedUserId = json_decode(file_get_contents("php://input"), true);
-
-    //     if(!empty($id) && 
-    //         !empty($connectedUserId) && 
-    //         is_numeric($id) && 
-    //         is_numeric($connectedUserId["userId"])){
-
-    //         if($id === intval($connectedUserId["userId"])){
-    //             http_response_code(400);
-    //             die('{"message":"Users are the same"}');
-    //         }
-            
-    //         $result = $userModel->checkIfUserAlreadyFollowing($id, $connectedUserId["userId"]);
-
-         
-    //         if(empty($result)){
-    //             http_response_code(202);
-    //             echo '{"message": "Not Following"}';
-    //         }
-
-    //         if(!empty($result)){
-    //             http_response_code(202);
-    //             echo '{"message": "Already Following"}';
-    //         }
-
-
-    //     } else{
-    //         http_response_code(400);
-    //         echo '{"message": "Bad Request"}';
-    //     }
-
-    // }
 
     else {
         http_response_code(405);
